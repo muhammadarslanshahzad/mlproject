348cf65e4eb4444c65b11fcde2930090b7206a3f
Muhammad Arslan Shahzad <itsmuhammadarslanshahzad@gmail.com>
2023-04-01 01:15:18 +0500

    testing logging and exception

--- src/exception.py
+++ src/exception.py
@@ -1,3 +1,4 @@
+import logging
 import sys
 
 def error_message_detail(error, error_detail:sys):
@@ -15,3 +16,10 @@ class CustomException(Exception):
     
     def __str__(self):
         return self.error_message
+    
+
+if __name__=="__main__":
+    try: 
+        a=1/0
+    except Exception as e:
+        raise CustomException(e, sys)
